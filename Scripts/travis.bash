da6c9c34ba545b78c63eb12ae4232b8b2a7f33ce
omochi <omochi.metaru@gmail.com>
2017-11-11 15:40:19 +0900

    travis

--- Scripts/travis.bash
+++ Scripts/travis.bash
@@ -2,13 +2,12 @@
 
 set -ue
 
-swift_version="swift-DEVELOPMENT-SNAPSHOT-2017-11-09-a"
-swift_package_name="${swift_version}-ubuntu14.04"
+swift_package_name="swift-4.0.2-RELEASE-ubuntu14.04"
 
 repo_dir="$(pwd)"
 cd "${repo_dir}/.."
 
-wget https://swift.org/builds/development/ubuntu1404/${swift_version}/${swift_package_name}.tar.gz
+wget https://swift.org/builds/swift-4.0.2-release/ubuntu1404/swift-4.0.2-RELEASE/${swift_package_name}.tar.gz
 tar xzf ${swift_package_name}.tar.gz
 export PATH="$(pwd)/${swift_package_name}/usr/bin:${PATH}"
 

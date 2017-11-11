dd3f252fb76c19dca11d7da6bbd1a23b4672662d
omochi <omochi.metaru@gmail.com>
2017-11-11 17:20:57 +0900

    travis

---
+++ Tests/GysbKitTest/SwiftCompatTest.swift
@@ -0,0 +1,51 @@
+//
+//  NSStringTest.swift
+//  GysbKitTest
+//
+//  Created by omochimetaru on 2017/11/11.
+//
+
+import XCTest
+import Foundation
+class SwiftCompatTest: XCTestCase {
+    func testPathAppend1() throws {
+        let actual = NSString(string: "").appendingPathComponentCompat("*.txt")
+        let expected = "*.txt"
+        XCTAssertEqual(actual, expected)
+    }
+    
+    func testPathAppend2() throws {
+        let actual = NSString(string: "a").appendingPathComponentCompat("*.txt")
+        let expected = "a/*.txt"
+        XCTAssertEqual(actual, expected)
+    }
+    
+    func testSubpaths1() throws {
+        let fm = FileManager.default
+        func isDir(_ str: String) -> Bool {
+            var y: ObjCBool = false
+            return fm.fileExists(atPath: str, isDirectory: &y) && y.boolValue
+        }
+        let dir = URL.init(fileURLWithPath: "TestResources/globstar")
+        
+        let actual: [String] = try fm
+            .subpathsOfDirectory(atPath: dir.path)
+            .filter { isDir(dir.appendingPathComponent($0).path) }
+            .sorted()
+        
+        let expected = [
+            "a",
+            "a/b",
+            "a/b/c"
+        ]
+        XCTAssertEqual(actual, expected)
+    }
+    
+    static var allTests: [(String, (SwiftCompatTest) -> () throws -> Void)] {
+        return [
+            ("testPathAppend1", testPathAppend1),
+            ("testPathAppend2", testPathAppend2),
+            ("testSubpaths1", testSubpaths1)
+        ]
+    }
+}
--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -2,11 +2,10 @@ import XCTest
 @testable import GysbKitTest
 
 XCTMain([
-
-    testCase(GlobTest.allTests),
+   testCase(SwiftCompatTest.allTests),
+//    testCase(GlobTest.allTests),
 //    testCase(ParserTest.allTests),
 //    testCase(TokenReaderTest.allTests),
 //    testCase(ConfigTest.allTests),
-//    testCase(NSStringTest.allTests),
 //    testCase(DriverTest.allTests),
 ])
